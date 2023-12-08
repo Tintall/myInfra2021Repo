c92510345336c9e7165424d204be7d603d3882b8
Tintall <143598866+Tintall@users.noreply.github.com>
2023-12-08 21:59:00 +0000

    Update sonar.tf

--- sonar.tf
+++ sonar.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "mySonarInstance" {
-      ami           = "ami-0ee23bfc74a881de5"
+      ami           = "ami-0fc5d935ebf8bc3bc"
       key_name = var.key_name
       instance_type = "t2.micro"
       vpc_security_group_ids = [aws_security_group.sonar-sg-2022.id]
